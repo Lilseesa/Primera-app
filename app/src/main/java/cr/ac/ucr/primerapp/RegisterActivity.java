5b704669d48295a124f759d5ce60e86dab279f19
Sven Seemann <svenebal@gmail.com>
2020-08-18 11:25:43 -0600

    Register

---
+++ app/src/main/java/cr/ac/ucr/primerapp/RegisterActivity.java
@@ -0,0 +1,50 @@
+package cr.ac.ucr.primerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText etName;
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etConfirmPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etName = findViewById(R.id.et_name);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        etConfirmPass = findViewById(R.id.et_confirm_password);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btn_register:
+                register();
+                break;
+            case R.id.btn_goto_login:
+                gotoLogin();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void register() {
+    }
+
+    private void gotoLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
